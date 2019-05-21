b1f4ffa82751dd7e7fbe9301dd34f2e8653f88b2
dongyi <juvenpp@gmail.com>
2019-05-21 17:34:04 +0800

    test lint

--- test.java
+++ test.java
@@ -1,7 +1,11 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        final double PI = 3.1415927;
+        try {
+           final double PI = 3.1415927;
+        } catch (RuntimeException ex) {
+
+        }
 
         System.out.println("Hello, World");
     }
