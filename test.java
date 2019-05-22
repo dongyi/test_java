99a6719d8adf6365dca9214d0563c5788b3137a1
dongyi <juvenpp@gmail.com>
2019-05-22 14:11:50 +0800

    test

--- test.java
+++ test.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         try {
            final double PI = 3.1415927;
+            PI = PI * 2;
         } catch (RuntimeException ex) {
 
         }
